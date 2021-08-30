292816e3fcedc08f1ef5b96b1c9a28475e183181
AstronGaming <yashudkl@Gmail.com>
2021-08-30 13:51:29 +0545

    Added time feature

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function prints(){
     document.getElementById("notgay").innerHTML = "I am not gay lol";
 }
+let a;
+let date;
+let time;
+setInterval(() => {
+a = new Date();
+date = a.toLocaleDateString();
+time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+document.getElementById("time").innerHTML = 'Time ' + time + ' Date ' +  date;
+}, 1000);
